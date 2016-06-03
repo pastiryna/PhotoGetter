4b0448fbaeb1c75f0c08521ec7b5ddfb3e1928b0
pastiryna <iryna.pastukhova@techmagic.co>
2016-06-03 16:46:12 +0300

    TextField animation

--- PhotoGetter/PhotoComments.swift
+++ PhotoGetter/PhotoComments.swift
@@ -28,14 +28,53 @@ class PhotoComments: BaseViewController, UITextFieldDelegate {
 
     }
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(PhotoComments.keyboardWillShowNotification(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(PhotoComments.keyboardWillHideNotification(_:)), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(true)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)    }
+    
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(true)
         self.navigationController?.navigationBar.hidden = false
-        self.bottomConstraint.constant = 400
+        ///self.bottomConstraint.constant = 400
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         self.commentField.resignFirstResponder()
+        self.bottomConstraint.constant = 0
         return true
     }
+    
+    func keyboardWillShowNotification(notification: NSNotification) {
+        updateBottomLayoutConstraintWithNotification(notification)
     }
+    
+    func keyboardWillHideNotification(notification: NSNotification) {
+        updateBottomLayoutConstraintWithNotification(notification)
+    }
+    
+    func updateBottomLayoutConstraintWithNotification(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        
+        let animationDuration = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+        let keyboardEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        let convertedKeyboardEndFrame = view.convertRect(keyboardEndFrame, fromView: view.window)
+        let rawAnimationCurve = (notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! NSNumber).unsignedIntValue << 16
+        let animationCurve = UIViewAnimationOptions(rawValue: UInt(rawAnimationCurve))
+        
+      
+        UIView.animateWithDuration(animationDuration, delay: 0.0, options: .BeginFromCurrentState, animations: {
+            self.bottomConstraint.constant = CGRectGetMaxY(self.view.bounds) - CGRectGetMinY(convertedKeyboardEndFrame)
+            }, completion: nil)
+    }
+    
+    
+
+
+}
